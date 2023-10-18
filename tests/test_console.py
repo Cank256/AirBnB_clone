7005e2f5fdde7dfaa0b8ca7896543b030da0000d
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-18 09:47:10 +0300

    more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,105 +1,60 @@
-#!/usr/bin/python3
 import unittest
 from unittest.mock import patch
-from io import StringIO
 from console import HBNBCommand
+import io
+import sys
 
 
-class TestConsole(unittest.TestCase):
+class ConsoleTest(unittest.TestCase):
+
     def setUp(self):
         self.console = HBNBCommand()
-        self.stdout = StringIO()
 
-    def tearDown(self):
-        self.stdout.close()
+    def test_create(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('create User')
+            self.assertEqual(f.getvalue(), '12345678-90ab-cdef-ghij-klmnopqrstuvwx\n')
 
-    def test_create_valid_class(self):
-        with patch('sys.stdout', new=self.stdout):
-            self.console.onecmd("create User")
-            output = self.stdout.getvalue().strip()
-            self.assertTrue(output.startswith("User."))
+    def test_show(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('show User 12345678-90ab-cdef-ghij-klmnopqrstuvwx')
+            self.assertEqual(f.getvalue(), '[User]\n')
 
-    def test_create_invalid_class(self):
-        with patch('sys.stdout', new=self.stdout):
-            self.console.onecmd("create InvalidClass")
-            output = self.stdout.getvalue().strip()
-            self.assertEqual(output, "** class doesn't exist **")
+    def test_destroy(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('destroy User 12345678-90ab-cdef-ghij-klmnopqrstuvwx')
+            self.assertEqual(f.getvalue(), '\n')
 
-    # Test case for the "show" command
-    def test_show_valid_instance(self):
-        with patch('sys.stdout', new=self.stdout):
-            # Create an instance of the class you want to test "show" on
-            self.console.onecmd("create User")
-            output = self.stdout.getvalue().strip()
-            instance_id = output.split(".")[1]
-            # Now, test the "show" command on this instance
-            self.console.onecmd(f"show User {instance_id}")
-            output = self.stdout.getvalue().strip()
-            self.assertTrue("User" in output)
-            self.assertTrue(instance_id in output)
+    def test_all(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('all User')
+            self.assertEqual(f.getvalue(), '[User]\n')
 
-    # Test case for the "destroy" command
-    def test_destroy_valid_instance(self):
-        with patch('sys.stdout', new=self.stdout):
-            # Create an instance of the class you want to test "destroy" on
-            self.console.onecmd("create User")
-            output = self.stdout.getvalue().strip()
-            instance_id = output.split(".")[1]
-            # Now, test the "destroy" command on this instance
-            self.console.onecmd(f"destroy User {instance_id}")
-            output = self.stdout.getvalue().strip()
-            self.assertEqual(output, "")
+    def test_update(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('update User 12345678-90ab-cdef-ghij-klmnopqrstuvwx name="Bard"')
+            self.assertEqual(f.getvalue(), '\n')
 
-    # Test case for the "all" command
-    def test_all_command(self):
-        with patch('sys.stdout', new=self.stdout):
-            # Test the "all" command for a specific class
-            self.console.onecmd("create User")
-            self.console.onecmd("create User")
-            self.console.onecmd("all User")
-            output = self.stdout.getvalue().strip()
-            self.assertTrue("User" in output)
-            self.assertTrue("1:" in output)
-            self.assertTrue("2:" in output)
+    def test_invalid_command(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('invalid_command')
+            self.assertEqual(f.getvalue(), 'Unknown command: invalid_command\n')
 
-    # Add test cases for the "update" command
-    def test_update_valid_instance(self):
-        with patch('sys.stdout', new=self.stdout):
-            # Create an instance of the class you want to test "update" on
-            self.console.onecmd("create User")
-            output = self.stdout.getvalue().strip()
-            instance_id = output.split(".")[1]
+    def test_help(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('help')
+            self.assertIn('create', f.getvalue())
+            self.assertIn('show', f.getvalue())
+            self.assertIn('destroy', f.getvalue())
+            self.assertIn('all', f.getvalue())
+            self.assertIn('update', f.getvalue())
 
-            # Now, test the "update" command on this instance
-            self.console.onecmd(f"update User {instance_id} first_name 'John'")
-            self.console.onecmd(f"show User {instance_id}")
-            output = self.stdout.getvalue().strip()
-            self.assertTrue("John" in output)
-
-    # Add test cases for the "default" function
-    def test_default_show(self):
-        with patch('sys.stdout', new=self.stdout):
-            self.console.onecmd("create User")
-            instance_id = self.stdout.getvalue().strip().split(".")[1]
-            self.stdout.truncate(0)
-            self.console.onecmd(f"User.show({instance_id})")
-            output = self.stdout.getvalue().strip()
-            self.assertTrue("John" in output)
-
-    def test_do_quit(self):
-        with patch('sys.stdout', new=self.stdout):
-            result = self.console.onecmd("quit")
-        self.assertTrue(result)  # Should return True to quit the program.
-
-    def test_do_EOF(self):
-        with patch('sys.stdout', new=self.stdout):
-            result = self.console.onecmd("EOF")
-        self.assertTrue(result)  # Should return True to quit the program.
-
-    def test_emptyline(self):
-        with patch('sys.stdout', new=self.stdout):
-            result = self.console.onecmd("")
-        self.assertIsNone(result)
+    def test_help_show(self):
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            self.console.onecmd('help show')
+            self.assertIn('Show the string representation of an instance', f.getvalue())
+            self.assertIn('Usage:', f.getvalue())
+            self.assertIn('Example:', f.getvalue())
 
 
 if __name__ == '__main__':
